6c2aadf0c6304615bfcbf9fab0c278b1a42ffee7
eunjeong <eunjeong990731@gmail.com>
2023-11-09 09:18:51 +0900

    1번과 2번 gpt 도움받아 작성

--- model/diff-logic.js
+++ model/diff-logic.js
@@ -1,28 +1,47 @@
+/**
+ * ? Q. JSON 파일을 아래의 5, 6번에 해당하는 로직 작성 후 JSON으로 저장
+ * ? Q. 저장이 완료되면 초기화된 result에 객체를 리턴
+ *
+ * * 1. inputJSONdata, outputJSONdata를 읽어서 JSON 객체로 변환
+ * * 2. inputJSONdata, outputJSONdata의 value를 비교
+ * * 3. outputJSONpath 매개변수의 key에 해당하는 정보를 저장
+ * * 4. dirrences.json 파일에 필요한 상태값
+ * * 5. 같은 단어가 무엇인지 저장
+ * * 6. 다른 단어가 무엇인지 저장
+ * * 7. 리턴을 통해 결과값을 전달
+ */
 
 /**
- * 
- * @param {JSON, Path} inputJSONdata 
- * @param {JSON, Path} outputJSONdata 
+ *
+ * @param {JSON, Path} inputJSONdata
+ * @param {JSON, Path} outputJSONdata
  * @returns Object
  */
 
-export default function(inputJSONPath, outputJSONPath) {
+import fs from 'fs';
+
+export default function (inputJSONPath, outputJSONPath) {
   if (!inputJSONPath.endsWith('.json') || !outputJSONPath.endsWith('.json')) {
-    throw new Error(`매개변수 ${inputJSONPath}, ${outputJSONPath}는 json 파일이 아닙니다.`);
+    throw new Error(
+      `매개변수 ${inputJSONPath}, ${outputJSONPath}는 json 파일이 아닙니다.`
+    );
   }
-  let result = {};
-  /**
-   * ? Q. JSON 파일을 아래의 5, 6번에 해당하는 로직 작성 후 JSON으로 저장
-   * ? Q. 저장이 완료되면 초기화된 result에 객체를 리턴
-   * 
-   * * 1. inputJSONdata, outputJSONdata를 읽어서 JSON 객체로 변환
-   * * 2. inputJSONdata, outputJSONdata의 value를 비교
-   * * 3. outputJSONpath 매개변수의 key에 해당하는 정보를 저장
-   * * 4. dirrences.json 파일에 필요한 상태값
-   * * 5. 같은 단어가 무엇인지 저장
-   * * 6. 다른 단어가 무엇인지 저장
-   * * 7. 리턴을 통해 결과값을 전달
-   */
+
+  // 1. JSON 파일을 읽어서 JSON 객체로 변환
+  const inputJSONData = JSON.parse(fs.readFileSync(inputJSONPath, 'utf8'));
+  const outputJSONData = JSON.parse(fs.readFileSync(outputJSONPath, 'utf8'));
+
+  // 2. inputJSONData와 outputJSONData의 value를 비교
+  const inputValues = Object.values(inputJSONData);
+  const outputValues = Object.values(outputJSONData);
+
+  // 이후의 로직을 구현하기 위해 변수를 초기화합니다.
+  // * 초기화를 하지 않아도 진행은 가능 하지만 초기화를 통해 변수를 명시적으로 설정하는 것은 코드의 가독성을 높이고 논리적인 오류를 방지 할 수 있고 다른 개발자가 보았을 때 가독성이 올라간다.
+
+  let result = {
+    sameWords: [],
+    differenceWords: [],
+  };
 
   return result;
 }
